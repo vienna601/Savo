0164e5727913c59f21509c66c1872fb8050de68d
vienna601 <vienna.sw.chiu@gmail.com>
2025-10-25 12:46:54 -0400

    setup2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Auth0Provider
+      // Use environment variables for Auth0 configuration
+      domain={import.meta.env.authDomain}
+      clientId={import.meta.env.authClientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
+);
